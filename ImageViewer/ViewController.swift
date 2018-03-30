ed4e0c81a04cfbaebd1eb6e66e456f81e804601c
Jared <jbates9210@gmail.com>
2018-03-30 13:16:43 -0400

    initial commit

--- ImageViewer/ViewController.swift
+++ ImageViewer/ViewController.swift
@@ -10,8 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("gc") {
+                pictures.append(item)
+            }
+        
+        print(pictures)
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
